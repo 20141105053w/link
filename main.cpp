a39802401eabd1dc973675ebc8b48a86ca47d048
20141105053w <20141105053w@admindeiMac-88.local>
2015-12-09 18:33:09 +0800

    link_student1
    与第一次提交的不同，它拥有自己的结构体名称，但是没有第一次的灵活，只适用于小程序

--- main.cpp
+++ main.cpp
@@ -7,32 +7,30 @@
 //
 
 #include <iostream>
-
 struct student{
     char name[100];
     int age;
-    struct student *next;
+    student * next;
     
 };
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
-    student *p,*head,*q;
-    int i;
-    head=new student;
-    head->next=0;
+    student A,B,C,D;
+    student *p,*head;
+    scanf("%s%d",A.name,&A.age);
+    scanf("%s%d",B.name,&B.age);
+    scanf("%s%d",C.name,&C.age);
+    scanf("%s%d",D.name,&D.age);
+    printf("%x %x %x %x\n",&A,&B,&C,&D);
+    A.next=&B;
+    B.next=&C;
+    C.next=&D;
+    D.next=0;
+    head=&A;
     p=head;
-    for(i=0;i<4;i++){
-        q=new student;
-        p->next=q;
-        q->next=0;
-        scanf("%s %d",q->name,&(q->age));
-        p=q;
-    }
-    p=head->next;
     while(p!=0)
     {
-        printf("%s %d p=%x \n",p->name,p->age,p);
+        printf("%s %d p=%x\n",p->name,p->age,p);
         p=p->next;
     }
     return 0;
